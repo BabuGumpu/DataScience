5132d35c60923f7244062e6fa825ad96628d1932
Gitansh Gumpu <gitanshgumpu@Gitanshs-MacBook-Pro.local>
2018-09-20 22:26:41 +0100

    first commit

---
+++ learn/python/floatequals.py
@@ -0,0 +1,8 @@
+# Float Equals Test
+a = 1.11 - 1.10
+b = 2.11 - 2.10
+c = 3.11 - 3.10
+
+print(bin(a))
+print(b)
+print(c)
---
+++ learn/python/indefiniteloop.py
@@ -0,0 +1,8 @@
+done = False
+
+while not done:
+    a = int(input())
+    if a == 10:
+        done = True
+    else:
+        print(a)
